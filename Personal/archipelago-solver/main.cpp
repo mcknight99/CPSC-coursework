fd610f90b377787d799736db9119d3a1c35200c6
mcknight99 <sampupke@gmail.com>
2025-03-09 17:31:30 -0400

    Update main.cpp

--- Personal/archipelago-solver/main.cpp
+++ Personal/archipelago-solver/main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+// log your own treasures as not possible for others to have
+// or just a command to dig an island away from everyone
+
+// log definitive places
+
+// fix command by color and player being separate boards??
+
 // future implementation desires:
 // if someone says true to a ring, mark the ring green as potential islands to get eliminated as options that could be loot islands
 
